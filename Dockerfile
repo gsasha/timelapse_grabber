50e53717ed503d2be2ddc39a766a280c6ef56b08
Sasha Gontmakher <gsasha@gmail.com>
2018-09-07 15:25:43 +0300

    Initial

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,16 @@ FROM gsasha/ffmpeg
 MAINTAINER Sasha <gsasha@gmail.com>
 LABEL Description="Grabber of stills from a wifi camera."
 
+RUN apk --no-cache add curl
 ADD grab_frame.sh /scripts/grab_frame.sh
-RUN mkdir -p /stills
+#RUN mkdir -p /stills
 VOLUME /stills
 
-ENV CameraName XXX
-ENV CameraIP 10.10.10.12
-ENV CameraUsername user
-ENV CameraPassword passwd
+ENV CAMERA_NAME caminion
+ENV CAMERA_ADDR 10.10.10.12:10554
+ENV PUSHGATEWAY_ADDR 10.10.10.17:9091
+ENV CAMERA_USER user
+ENV CAMERA_PASSWORD passwd
 
 RUN echo "* * * * * /usr/bin/nohup /scripts/grab_frame.sh" > /etc/crontabs/root
 CMD '/usr/sbin/crond' '-f'
--- grab_frame.sh
+++ grab_frame.sh
@@ -1,35 +1,47 @@
 #!/bin/sh
 
 # TimeLapse snapshot capture script.
+# Captures data
 
-# This script should capture any stills in the incoming Stills directory
+storage_directory=/stills
+mkdir -p $storage_directory/$CAMERA_NAME
+log_file=$storage_directory/$CAMERA_NAME/log.txt
+
+if [ ! -t 0 ]; then
+	exec > $log_file 2>&1
+fi
 
 time=`date '+%Y_%m_%d__%H_%M_%S'`;
+storage_filename=$CAMERA_NAME-$time.png
+storage_file=$storage_directory/$storage_filename
 
-# The 4 parameters below should be provided in Docker environment vars.
-#CameraName='camname'
-#CameraIP='10.10.1.5'
-#CameraUsername='user'
-#CameraPassword='passwd'
-
-# Shouldn't need to be changed!
-StorageDirectory=/stills
-StorageFilename=$CameraName-$time.png
-
-mkdir -p $StorageDirectory/$CameraName;
-
-#echo ffmpeg -rtsp_transport udp -i rtsp://$CameraUsername:$CameraPassword@$CameraIP:10554/udp/av0_0 -ss 00:00:00.50 -frames:v 1 $StorageDirectory/$CameraName/$StorageFilename -loglevel debug >> /stills/log.txt 
-
-attempts=`cat $StorageDirectory/$CameraName/attempts`
+# Record an attempt to grab a frame.
+attempts=`cat $storage_directory/$CAMERA_NAME/attempts`
 attempts=$((attempts+1))
-echo -n $attempts > '$StorageDirectory/$CameraName/attempts'
+echo -n $attempts > "$storage_directory/$CAMERA_NAME/attempts"
 
-successes=`cat $StorageDirectory/$CameraName/successes`
-successes=$((successes+1))
+# Attempt to grab a frame
+successes=`cat $storage_directory/$CAMERA_NAME/successes`
 
-timeout 5 ffmpeg -rtsp_transport udp -i rtsp://$CameraUsername:$CameraPassword@$CameraIP:10554/udp/av0_0 -ss 00:00:00.50 -frames:v 1 $StorageDirectory/$CameraName/$StorageFilename -loglevel info >> /stills/log.txt 2>&1
+timeout -t 5 ffmpeg -rtsp_transport udp -i rtsp://$CAMERA_USER:$CAMERA_PASSWORD@$CAMERA_ADDR/udp/av0_0 -ss 00:00:00.50 -frames:v 1 $storage_directory/$CAMERA_NAME/$storage_filename -loglevel info
 
-if ! $? ; then
+retcode=$?
+if [ $retcode -ne 0 ] ; then
+	echo "Grab failed"
+	successes=$((successes+0))
+else
 	echo "Grab succeeded"
-	echo -n $successes > '$StorageDirectory/$CameraName/attempts'
+        successes=$((successes+1))
+fi
+echo -n $successes > "$storage_directory/$CAMERA_NAME/successes"
+
+# Report stats to pushgateway.
+if [ ! -z $PUSHGATEWAY_ADDR ]; then
+	echo "Reporting stats to $PUSHGATEWAY_ADDR"
+	cat<<EOF | curl --data-binary @- http://$PUSHGATEWAY_ADDR/metrics/job/timelapse_grabber/instance/$CAMERA_NAME
+# TYPE attempts counter
+attempts $attempts
+# TYPE successes counter
+successes $successes
+EOF
 fi
